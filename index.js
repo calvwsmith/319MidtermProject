8d82d0e8cdd52c0cf16c1dd6927ab08c979adaed
unknown <calvwsmith@gmail.com>
2024-03-09 00:02:43 -0600

    added stuff to the json, as well as added some functino calls and the script to each html file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import data from './data.json';
+
 function toggleElementVisabilityById(id){
     let ele = document.getElementById(id);
     if (ele.style.display == "block"){
@@ -17,6 +19,13 @@ function toggleElementVisabilityByClass(clas){
         ele.style.display = "block";
     }
 }
-function InsertJsonElementsById(id){
-    
-}
+
+document.getElementById("audio1").innerHTML = data.audio1.link;
+document.getElementById("audio1").innerHTML = data.audio1.text;
+document.getElementById("audio1").innerHTML = data.audio1.title;
+document.getElementById("audio2").innerHTML = data.audio2.link;
+document.getElementById("audio2").innerHTML = data.audio2.text;
+document.getElementById("audio2").innerHTML = data.audio2.title;
+document.getElementById("audio3").innerHTML = data.audio3.link
+document.getElementById("audio3").innerHTML = data.audio3.text;
+document.getElementById("audio3").innerHTML = data.audio3.title;
